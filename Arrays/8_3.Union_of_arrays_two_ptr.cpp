1b2cce0b3499d0f18df3600a2cc9d4f0ecfeb0d1
Mihir2104 <rakhondemihir@gmail.com>
2026-04-02 23:12:26 +0530

    union of array using two pointers

---
+++ Arrays/8_3.Union_of_arrays_two_ptr.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> unionArray(vector<int>& nums1, vector<int>& nums2) {
+
+    int x = nums1.size();
+    int y = nums2.size();
+
+    vector<int> result;
+
+    int i = 0, j = 0;
+
+    while (i < x && j < y) {
+        if (nums1[i] < nums2[j]) {
+            if (result.empty() || result.back() != nums1[i])
+                result.push_back(nums1[i]);
+            i++;
+        }
+        else if (nums1[i] > nums2[j]) {
+            if (result.empty() || result.back() != nums2[j])
+                result.push_back(nums2[j]);
+            j++;
+        }
+        else {
+            if (result.empty() || result.back() != nums1[i])
+                result.push_back(nums1[i]);
+            i++;
+            j++;
+        }
+    }
+
+    while (i < x) {
+        if (result.empty() || result.back() != nums1[i])
+            result.push_back(nums1[i]);
+        i++;
+    }
+
+    while (j < y) {
+        if (result.empty() || result.back() != nums2[j])
+            result.push_back(nums2[j]);
+        j++;
+    }
+
+    return result;
+}
+
+int main() {
+
+    vector<int> nums1 = {1, 1, 2, 3, 4};
+    vector<int> nums2 = {2, 2, 3, 5};
+
+    vector<int> result = unionArray(nums1, nums2);
+
+    cout << "Union Array: ";
+    for (int x : result) {
+        cout << x << " ";
+    }
+
+    return 0;
+}
